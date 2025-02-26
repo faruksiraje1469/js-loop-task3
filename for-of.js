482bf07afd6744e584acec45a0dd830050d00c6d
faruksiraje1469 <faruksiraje@gmail.com>
2025-02-26 21:54:23 +0600

    first commit

---
+++ for-of.js
@@ -0,0 +1,13 @@
+// const numbers = [12, 14, 16, 18, 20];
+// console.log(numbers);
+
+// for (const num of numbers) {
+//   console.log(num);
+// }
+
+const fruits = ['apple', 'banana', 'mango', 'orange', 'papaya'];
+
+for (const fruit of fruits) {
+  console.log(fruit);
+  console.log("i want to eat");
+}
